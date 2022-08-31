04a0e21c63c2e880b42b416d35748f779bed1f59
XZACTA <oak4312@gmail.com>
2022-09-01 02:14:55 +0700

    work3 php

---
+++ post.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 align="center">Web loam</h1>
+    <hr>
+    <br>
+    <div align="center"> ต้องการดูกระทู้หมายเลข <?php echo $_GET["id"]; ?> 
+    
+    </div>
+    <br>
+    <table style="border:2px solid black; width: 40%" align="center">
+        <tr><td  style="background-color: #6CD2FE ;"colspan="2">แสดงความคิดเห็น</td></tr>
+        <tr><td><textarea name="" id="" cols="70%" rows="10"></textarea></td></tr>
+        <tr><td colspan="2" align="center"><input type="submit" value="ส่งข้อความ"></td></tr>
+        
+    </table>
+    <br>
+    <a href="index.html"><center>กลับไปหน้าหลัก</center></a>
+    
+</body>
+</html>
